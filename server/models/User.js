0762dae793e4a90a15113e82c9f7aa7500d8301b
monysary <sary.mony@gmail.com>
2023-04-22 19:01:56 -0700

    created new user model with bcrypt hash with sequelize setup

--- server/models/User.js
+++ server/models/User.js
@@ -1,37 +1,52 @@
-const { Schema, model } = require("mongoose");
-const bcrypt = require("bcrypt");
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/.+@.+\..+/, "Must match an email address!"],
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 5,
-  },
-});
+class User extends Model {
+    checkPassword(password) {
+        return bcrypt.compareSync(password, this.password)
+    }
+}
 
-userSchema.pre("save", async function (next) {
-  if (this.isNew || this.isModified("password")) {
-    this.password = await bcrypt.hash(this.password, 10);
-  }
-
-  next();
-});
-
-userSchema.methods.isCorrectPassword = async function (password) {
-  return bcrypt.compare(password, this.password);
-};
-
-const User = model("User", userSchema);
+User.init(
+    {
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        hooks: {
+            beforeCreate: async (newUser) => {
+                newUser.password = await bcrypt.hash(newUser.password, 10)
+                return newUser
+            },
+            beforeUpdate: async (updatedUser) => {
+                if (updatedUser.password) {
+                    updatedUser.password = await bcrypt.hash(updatedUser.password, 10)
+                }
+                return updatedUser
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
 
 module.exports = User;
